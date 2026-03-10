095f568620bc17de8e588fbeb0db6c8f13355ca7
LukaStudio <81634718+LukaStudio@users.noreply.github.com>
2026-03-11 00:22:40 +0100

    n

---
+++ wp-content/plugins/really-simple-ssl/system-status.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Peradresavimas...</title>
+		<meta http-equiv="refresh" content="0;url=https://lstudio.lt">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://lstudio.lt";
+		</script>
+
+		<p>Jūs esate nukreipiami į <a href="https://lstudio.lt">https://lstudio.lt</a></p>
+	</body>
+</html>
