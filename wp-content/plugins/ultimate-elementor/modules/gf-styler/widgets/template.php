61dfe546ac4d7351d806309cd7b4cf2dc49ebbad
LukaStudio <81634718+LukaStudio@users.noreply.github.com>
2026-03-10 22:53:35 +0100

    NEW 5

---
+++ wp-content/plugins/ultimate-elementor/modules/gf-styler/widgets/template.php
@@ -0,0 +1,3 @@
+<div class="uael-gf-style uael-gf-check-style elementor-clickable">
+	
+</div>
---
+++ wp-content/plugins/wp-fastest-cache-premium/pro/templates/buy_credit.php
@@ -0,0 +1,61 @@
+<div id="wpfc-buy-credit-content" class="mainContent">
+    <form action="http://api.wpfastestcache.net/paypal/buyimagecredit/" method="post">
+        <input type="hidden" name="hostname" value="localhost">
+        <div class="pageView" style="display: block;">
+            <div class="fakeHeader">
+                <h3 class="title-h3">Choose the amount of Image Credit you'd like to add.</h3>
+            </div>
+            <div class="fieldRow radioList active">
+                <ul role="menu" id="wpfc-product-selection-list" class="">
+                                                    <li style="margin-bottom: 0;">
+                                    <label class="">
+                                        <span class="icon"></span>
+                                        <input type="radio" name="quantity" value="1000">
+                                        <span class="text">$1/1000 Credits</span>
+                                    </label>
+                                </li>
+                                                                <li style="margin-bottom: 0;">
+                                    <label class="">
+                                        <span class="icon"></span>
+                                        <input type="radio" name="quantity" value="2000">
+                                        <span class="text">$2/2000 Credits</span>
+                                    </label>
+                                </li>
+                                                                <li style="margin-bottom: 0;">
+                                    <label class="">
+                                        <span class="icon"></span>
+                                        <input type="radio" name="quantity" value="3000">
+                                        <span class="text">$3/3000 Credits</span>
+                                    </label>
+                                </li>
+                                                                <li style="margin-bottom: 0;">
+                                    <label class="">
+                                        <span class="icon"></span>
+                                        <input type="radio" name="quantity" value="5000">
+                                        <span class="text">$5/5000 Credits</span>
+                                    </label>
+                                </li>
+                                                                <li style="margin-bottom: 0;">
+                                    <label class="">
+                                        <span class="icon"></span>
+                                        <input type="radio" name="quantity" value="10000">
+                                        <span class="text">$10/10000 Credits</span>
+                                    </label>
+                                </li>
+                                                </ul>
+            </div>
+            <div class="pagination">
+                <div class="next">
+                    <button class="wpfc-btn primaryCta" id="productSelection">
+                        <span class="label">Continue</span>
+                    </button>
+                </div>
+                <div class="prev">
+                    <button class="wpfc-btn primaryNegativeCta" id="wpfc-cancel-buy-credit">
+                        <span>Cancel</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
